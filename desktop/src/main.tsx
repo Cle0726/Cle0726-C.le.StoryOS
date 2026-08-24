88415e1c9deea1e3bce78943643afb98faa23952
Cle0726 <kk07266417@gmail.com>
2026-08-24 20:36:59 +0800

    style: load StoryOS product shell styles

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './writer.css';
+import './product.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
